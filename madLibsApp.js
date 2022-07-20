4bf2a95760d696e6d132e29da1814bf5c132c2db
Reinaldo <khillgamer18@gmail.com>
2022-07-20 16:44:45 -0300

    Added mad-lib replacement variables to story

---
+++ madLibsApp.js
@@ -0,0 +1,5 @@
+"user strict"
+let originalStory= `Hi i am ${userInputs[0]}, i born in ${userInputs[1]}, and i love ${userInputs[2]}, my objetive in this my history is ${userInputs[3]},`;
+let userInputs = [];
+console.log(originalStory);
+//`Hi i am, i born in, and i love, my objetive in this my history is,`;
