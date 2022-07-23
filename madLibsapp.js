cc6b6857e89508be39575243c66041ab63b33ab7
Reinaldo <khillgamer18@gmail.com>
2022-07-23 12:11:55 -0300

    numberOfQuestions

--- madLibsapp.js
+++ madLibsapp.js
@@ -1,5 +1,6 @@
 "user strict"
 let originalStory= `Hi i am ${userInputs[0]}, i born in ${userInputs[1]}, and i love ${userInputs[2]}, my objetive in this my history is ${userInputs[3]},`;
 let userInputs = [];
+let numberOfQuestions = 4;
 console.log(originalStory);
 //`Hi i am, i born in, and i love, my objetive in this my history is,`;
