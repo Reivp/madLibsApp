1a19813d383a738f67211356a3dc5f4210070244
Reinaldo <khillgamer18@gmail.com>
2022-07-27 18:25:09 -0300

    removed forEach and moved some things around mad-lib

--- madLibsapp.js
+++ madLibsapp.js
@@ -5,30 +5,31 @@ the main idea is to show the user message and take it
 to the text again and again to all the gaps in the text and complete.
 */
 
-/* Here I write the story joining the questionray and the user msgs. */
-let originalStory = `${questionArray[0]} ${userInputs[3]} ${questionArray[1]} ${userInputs[2]} ${questionArray[2]} ${userInputs[1]} ${questionArray[3]} ${userInputs[0]}`;
-
 /* Here I create the place where the user's messages will be deposited. */
-let userInputs = [`${userinputs} A name`,`${userInputs} A city`,`${userInputs} A Word`,`${userInputs} You are?`];
-
-/* Here I start to create a repetition system with forEach */ 
-let numberOfQuestions = 3;
-
-loop.forEach(()=>
-{
-
-/* Inside I create the command that will allow the user to give the answer */
-    p1.push(prompt(userInputs[numberOfQuestions]) ,numberOfQuestions--)
-/* For log verification purposes */
-    for (let i=numberOfQuestions;i>=0;i--)
-{
-    questionCounter++
-    console.log(i);
-}let questionCounter = 0;
-})
+let userInputs = [`A name`,`A city`,`A Word`,`You are?`];
 
 /* The strings used in the final text. */
-questionArray = [`Hi i am `,`, I born in `,` and i love `,`, my objetive in this my history is `];
+let questionArray = [`Hi i am `,`, I born in `,` and i love `,`, my objetive in this my history is `];
+
+/*
+   --Here I start to create a repetition system with forEach
+   --I undid mistakes and left it in a way that can run with my humble knowledge ;-;
+   
+   --What I understood from the problem: 
+   --
+   --when I do it inside the for it can't associate 
+   --that the word must go to the array before going to the next prompt
+*/
+
+let word0 = prompt(userInputs[0]);
+let word1 = prompt(userInputs[1]);
+let word2 = prompt(userInputs[2]);
+let word3 = prompt(userInputs[3]);
+
+let arrayWord = [word0,word1,word2,word3];
+
+/* Here I write the story joining the questionray and the user msgs. */
+let originalStory = `${questionArray[0]} ${arrayWord[0]} ${questionArray[1]} ${arrayWord[1]} ${questionArray[2]} ${arrayWord[2]} ${questionArray[3]} ${arrayWord[3]}`;
 
 /* The Final Text */    
 console.log(originalStory);
